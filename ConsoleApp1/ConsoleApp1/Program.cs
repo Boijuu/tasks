fa0d4d0b2eb7f9edc9d4fb0d964f7b881bfa77d0
Boijuu <anton.peiju@gmail.com>
2022-11-07 14:01:34 +0200

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tapan artun");
+            Console.WriteLine("kun seuraavan kerran tapan, niin tapan myös kaikki jota hän rakastaa");
+            Console.WriteLine("viimeisessä kamppailussa poliisia vastaan surmaan 18 siviiliasuista poliisia ja otan oman henkeni samalla");
+            Console.WriteLine(@"mutta koenkin hengellisen rajakuolema kokemuksen,
+jonka jälkeen palaan tajuinihi ja uskon jeesukseen ja kaikki syntini annetaan anteeksi.
+Kuristan sellitoverini yöllä kello 3:14 12 päivä joulukuuta"
+        }
+    }
+}
---
+++ pizza/pizza/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pizza on valmis.");
+            Console.WriteLine("Kenelle Pizza on?");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Kirjataan muistiin. Pizza menee henkilölle " + nimi);
+        }
+    }
+}   
+
