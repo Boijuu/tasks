80e4cd9d334c380187cc30fc96b96b82eeb16659
Boijuu <72388103+Boijuu@users.noreply.github.com>
2022-11-15 15:21:18 +0200

    Add files via upload

---
+++ tehtava6/tehtava6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tehtava6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa mikä pizza saisi olla");
+         
+            
+            // listan alku ja jokainen string tarkoittaa eri pizzaa.
+            string yksi = "Hawaii";
+            string kaksi = "pizza2";
+            string kolme = "pizza3";
+            string neljä = "pizza4";
+            string viisi = "pizza5";
+            string kuusi = "pizza6";
+            string seitsemän = "pizza7";
+            string kahdeksan = "pizza8";
+            string yhdeksän = "pizza9";
+            string kymmenen = "pizza10";
+            string yksitoista = "pizza11";
+            string kaksitoista = "pizza12";
+            string kolmetoista = "pizza13";
+            string neljätoista = "pizza14";
+            Console.WriteLine(yksi + kaksi + kolme + neljä + viisi + kuusi + seitsemän + kahdeksan + yhdeksän + kymmenen + yksitoista + kaksitoista + kolmetoista + neljätoista);
+        }
+    }
+}
