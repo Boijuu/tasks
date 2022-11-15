025a9471cd3afe0955635162d3262387e5daf848
Boijuu <72388103+Boijuu@users.noreply.github.com>
2022-11-15 14:16:21 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+
+namespace MunApplikaatio
+
+{
+
+    class Auto
+
+    {
+
+        string merkki;
+
+        string vari;
+
+        int vuosi;
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            Auto ekaAutoni = new Auto();
+
+            Auto tokaAutoni = new Auto();
+
+            Auto vikaAutoni = new Auto();
+
+
+
+            ekaAutoni.merkki = "Audi";
+
+            ekaAutoni.vari = "punainen";
+
+            ekaAutoni.vuosi = 2010;
+
+
+
+            tokaAutoni.merkki = "Honda";
+
+            tokaAutoni.vari = "vihreä";
+
+            tokaAutoni.vuosi = 2018;
+
+
+
+            vikaAutoni.merkki = "Volvo";
+
+            vikaAutoni.vari = "keltainen";
+
+            vikaAutoni.vuosi = 2020;
+
+        }
+
+    }
+
+}
---
+++ The robot namer/The robot namer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace The_robot_namer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of robot are we talking about?");
+            string a = Console.ReadLine(); // tämä muuttuja sisältää merkkijonon joka printataan viimeisessä osassa koodia
+            Console.WriteLine("how would you describe it?");
+            string b = Console.ReadLine(); // tämä muuttuja sisältää merkkijonon joka printataan viimeisessä osassa koodia
+            string c = "Of destruction";  // tämä muuttuja sisältää merkkijonon joka printataan viimeisessä osassa koodia
+            string d = "5000"; // tämä muuttuja sisältää merkkijonon joka printataan viimeisessä osassa koodia 
+            Console.WriteLine("the " + b + " " + a + " Bot " + c + " " + d + "!"); 
+        }
+
+    }
+}
