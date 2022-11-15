d1e9b0bd538b2b8a684e3c3e3b7b2436b3bef184
Boijuu <72388103+Boijuu@users.noreply.github.com>
2022-11-15 19:31:02 +0200

    Add files via upload

---
+++ tehtava7/tehtava7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tehtava7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjota kolmion kanta ja korkeus senttimetreina:");
+            int kanta = Convert.ToInt32(Console.ReadLine());
+            int korkeus = Convert.ToInt32(Console.ReadLine());
+            double pintaala = (double)kanta * korkeus / 2;
+            Console.WriteLine("Pinta-ala on " + pintaala + "cm^2");
+        }
+    }
+}
