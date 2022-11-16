c01cd5f9741d9e59a35e562b5920ee0393cb5750
Boijuu <72388103+Boijuu@users.noreply.github.com>
2022-11-16 13:02:18 +0200

    Add files via upload

---
+++ tehtava10/tehtava10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tehtava10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kohde rivi? ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Kohde sarake? ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lahetetaan ritarit.");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Console.Beep();
+
+            Console.WriteLine("Ritari yksi kohteeseen (" + x + ", " + (y - 1) + ")");
+
+            Console.WriteLine("Ritari kaksi kohteeseen (" + (x - 1) + ", " + y + ")");
+
+            Console.WriteLine("Ritari kolme kohteeseen (" + x + ", " + (y + 1) + ")");
+
+            Console.WriteLine("Ritari nelja kohteeseen (" + (x + 1) + ", " + y + ")");
+        }
+    }
+}
