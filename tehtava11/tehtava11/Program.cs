886bd52dbb30b58a704c22e794a15f580ceae88d
Boijuu <72388103+Boijuu@users.noreply.github.com>
2022-11-16 13:07:35 +0200

    Add files via upload

---
+++ tehtava11/tehtava11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tehtava11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syota numero: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+            }
+
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+
+        }
+    }
+}
